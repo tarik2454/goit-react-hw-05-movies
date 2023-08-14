69b6530c9de28c78ab04440301c58d9e75c6c1d7
tarik2454 <tarik2454@gmail.com>
2023-08-14 12:49:42 +0000

    Deploying to gh-pages from @ tarik2454/goit-react-hw-05-movies@d5554eb0d46d8782534a4648dbb644d34dd93f22 🚀

---
+++ static/js/805.aee8c4d4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:"NotFound"})}}}]);
+//# sourceMappingURL=805.aee8c4d4.chunk.js.map
